b1f50f998c3e9aca35d50c1c418dfe51485f52d4
jonson <jonson.ncube@gmail.com>
2020-11-25 14:33:42 +0200

    Shopify dependencies added

--- src/redux/shopping/shopActions.js
+++ src/redux/shopping/shopActions.js
@@ -1,4 +1,18 @@
 import shopTypes from './shopTypes'
+import Client from 'shopify-buy';
+
+
+// Initializing a client to return content in the store's primary language
+const client = Client.buildClient({
+    domain: 'benjamin-woollens.myshopify.com',
+    storefrontAccessToken: '376eecf28db5113a0f99f4766adecef7'
+});
+
+// Fetch all products in your shop
+client.product.fetchAll().then((products) => {
+    // Do something with the products
+    console.log(products);
+});
 
 export const addToCart = (productName) => {
 
