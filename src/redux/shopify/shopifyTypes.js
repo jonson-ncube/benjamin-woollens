70e4f3fd85aa19f2bcfa4245452d9c4f77827857
Jonson Ncube <36877522+jonson-ncube@users.noreply.github.com>
2020-11-28 19:25:56 +0200

    Create shopifyTypes.js

---
+++ src/redux/shopify/shopifyTypes.js
@@ -0,0 +1,8 @@
+const shopifyTypes = {
+    FETCH_ALL_PRODUCTS: 'FETCH_ALL_PRODUCTS',
+    CREATE_CHECKOUT: 'CREATE_CHECKOUT',
+    FETCH_PRODUCT_ID: 'FETCH_PRODUCT_ID',
+    ADD_ITEM_TO_CART: 'ADD_ITEM_TO_CART'
+}
+
+export default shopifyTypes
